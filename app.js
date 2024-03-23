57137d61ec5e270f486816de1242eae313319d46
Connex Digital <clientaccess@connex.digital>
2024-03-23 08:10:01 -0400

    first commit

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(require('./routes/share'));
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
-});
+}); 
 
 // error handler
 app.use(function (err, req, res, next) {
